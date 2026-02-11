e4da67efada8fdb6a91fb9a8388387b7282e8a2e
robhendriks <cyanidenl@icloud.com>
2026-02-11 11:39:06 +0100

    feat(init): add args to set initial version and/or author

--- muscle/src/command/init.rs
+++ muscle/src/command/init.rs
@@ -1,35 +1,45 @@
 use std::path::{Path, PathBuf};
 
 use clap::Args;
+use clap::builder::Str;
 use muscle_core::{json::JsonContainer, module::ModuleJson, project::ProjectJson};
 
 use crate::cli::Cli;
 
 #[derive(Debug, Args)]
 pub struct InitArgs {
+    #[arg(short, long, default_value = "**/main.bicep")]
+    glob: String,
+
     #[arg(short, long, default_value_t = false)]
     force: bool,
+
+    #[arg(short, long, default_value = "John Doe")]
+    author: String,
+
+    #[arg(short, long, default_value = "0.1.0")]
+    version: String,
 }
 
 impl InitArgs {
     pub async fn execute(&self, cli: &Cli) -> anyhow::Result<()> {
-        init_project(&cli.root, self.force).await?;
-        init_modules(&cli.root, self.force).await
+        init_project(&cli.root, &self).await?;
+        init_modules(&cli.root, &self).await
     }
 }
 
-async fn init_project(root: &Path, overwrite: bool) -> anyhow::Result<()> {
+async fn init_project(root: &Path, args: &InitArgs) -> anyhow::Result<()> {
     let json_path = ProjectJson::get_path(root);
     let json_c = JsonContainer::from(&json_path, ProjectJson {});
 
-    let result = json_c.write_safe(overwrite).await?;
+    let result = json_c.write_safe(args.force).await?;
 
     log::info!("[{:?}] {}", result, json_path.display());
     Ok(())
 }
 
-async fn init_modules(root: &Path, overwrite: bool) -> anyhow::Result<()> {
-    let module_discover_path = root.join("**/bicep/main.bicep");
+async fn init_modules(root: &Path, args: &InitArgs) -> anyhow::Result<()> {
+    let module_discover_path = root.join(&args.glob);
     let module_discover_str = module_discover_path.to_str().unwrap();
 
     let module_paths = discover_modules(module_discover_str)?;
@@ -41,15 +51,15 @@ async fn init_modules(root: &Path, overwrite: bool) -> anyhow::Result<()> {
         let json_c = JsonContainer::from(
             &json_path,
             ModuleJson {
-                name: String::from("my-module"),
-                description: String::from("My module"),
-                authors: vec![String::from("John Doe")],
-                version: String::from("0.1.0"),
+                name: String::from(""),
+                description: String::from(""),
+                authors: vec![args.author.to_string()],
+                version: args.version.to_string(),
                 main: String::from(module_main_file_name.to_str().unwrap()),
             },
         );
 
-        let result = json_c.write_safe(overwrite).await?;
+        let result = json_c.write_safe(args.force).await?;
 
         log::info!("[{:?}] {}", result, json_path.display());
     }
